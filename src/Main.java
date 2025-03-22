f4e5f7382b613f4bbf0856e04b9aa3071ad5f029
Alexandr Jordan <step_up_tomsk@mail.ru>
2025-03-22 19:30:00 +0700

    Добавлен парсер файлов

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.io.File;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: " );
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        Scanner scanner = new Scanner(System.in);
+        int fileCount = 0;
+
+        while (true) {
+            System.out.println("Введите путь к файлу и нажмите <Enter>: ");
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Ошибка: указанный путь не существует или является папкой. Попробуйте снова.");
+                continue;
+            }
+
+            fileCount++;
+            System.out.println("Путь указан верно.");
+            System.out.println("Это файл номер " + fileCount);
         }
     }
+}
